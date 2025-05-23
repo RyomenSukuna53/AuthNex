304506172dc80032d1d5a466035f598140bebffb
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-05-23 15:30:53 +0530

    Create Database.py

---
+++ AuthNex/Database.py
@@ -0,0 +1,7 @@
+from motor.motor_asyncio import AsyncIOMotorClient 
+
+MONGO_URI = "mongodb+srv://sufyan532011:2011@authnex.nvjbscr.mongodb.net/?retryWrites=true&w=majority&appName=AuthNex"
+Database = AsyncIOMotorClient(MONGO_URI)
+AuthNex = Database["AuthNex"]
+user_col = AuthNex["USER_LOGINS"]
+ban_col = AuthNex["BANNED_USERS"]
