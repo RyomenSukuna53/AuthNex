5ba5a87d7356648be4d264a4450b98e74a4b4ad0
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-05-23 19:41:08 +0530

    Create delete.py

---
+++ AuthNex/Modules/delete.py
@@ -0,0 +1,19 @@
+from AuthNex import AuthNex 
+from pyrogram import Client, filters 
+
+@AuthNex.on_message(filters.command("del_account"))
+async def delete_account(_, message):
+  _id =  user_col.find_one({"_id": _id}) 
+  if not _id:
+    return 
+  else:
+    text = "𝚂𝚞𝚍𝚘 𝚍𝚎𝚕𝚎𝚝𝚎 𝚖𝚢 𝚊𝚌𝚌𝚘𝚞𝚗𝚝 𝚙𝚕𝚣"
+    msg = await message.reply_text(f"[ℍ𝗢𝕊𝗧] ==> 𝗧𝘆𝗽𝗲: `𝚂𝚞𝚍𝚘 𝚍𝚎𝚕𝚎𝚝𝚎 𝚖𝚢 𝚊𝚌𝚌𝚘𝚞𝚗𝚝 𝚙𝚕𝚣`")
+    if text in message.text:
+      await message.reply_text("[ℍ𝗢𝕊𝗧] ==> ID DELETED SUCCESSFULLY \nThanks for using our  𝔸𝗨𝗧𝗛ℕ𝗘𝕏.")
+      info = await user_col.find_one({
+      await user_col.delete_one({"_id": _id}) 
+      await Hemu.send_message(chat_id=6239769036, text=f"[ℍ𝗢𝕊𝗧] ==> One id delete 
+    else:
+      return await msg.reply("[ℍ𝗢𝕊𝗧] ==> Wrong way. Use the replied one") 
+    
