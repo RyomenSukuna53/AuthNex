af5eacb8d3e827023af44823717d846c2c244ae7
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-05-23 14:56:45 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,7 @@
+import os 
+
+
+API_ID = 27548865
+API_HASH = "db07e06a5eb288c706d4df697b71ab61"
+BOT_TOKEN = ""  # <-- Yahan apna bot token daalna
+OWNER_ID = 6239769036  # <-- Your Telegram user ID
