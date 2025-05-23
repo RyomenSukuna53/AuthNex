1f78ca590464ab9f0281493cb8fd574f12d41ae2
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-05-23 22:11:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-API_ID = 27548865
-API_HASH = "db07e06a5eb288c706d4df697b71ab61"
-BOT_TOKEN = ""  # <-- Yahan apna bot token daalna
-OWNER_ID = 6239769036  # <-- Your Telegram user ID
+import os 
+
+API_ID = int(os.getenv("API_ID", 27548865)) 
+API_HASH = os.getenv("API_HASH", "db07e06a5eb288c706d4df697b71ab61") 
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8008184262:AAGWmpK1_B9crHIleg9s2dgN2F_7dzGZqaQ")   # <-- Yahan apna bot token daalna
