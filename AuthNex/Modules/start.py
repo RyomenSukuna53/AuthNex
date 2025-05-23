ac2f53f2731a018b8faf07a45e37acc1dcb17515
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-05-23 15:25:21 +0530

    Create start.py

---
+++ AuthNex/Modules/start.py
@@ -0,0 +1,15 @@
+from pyrogram import Client, filters 
+from AuthNex import AuthNex 
+
+@AuthNex.on_message(filters.command("start")) 
+async def start_commands(_, message):
+  await message.reply_photo(photo="https://files.catbox.moe/vw9cip.jpg",caption="""|  𝔸𝗨𝗧𝗛ℕ𝗘𝗫  |
+
+We offers the best accounts to play game bots and use workers created by [𝔸𝗨𝗧𝗛ℕ𝗘𝗫].
+Enjoy reliable performance (⁠◍⁠•⁠ᴗ⁠•⁠◍⁠)
+
+Owner: @M15T3R_C0D3R
+Ceo: @Uzumaki_X_Naruto6
+
+[𝗡𝗢𝗧𝗘] Use /register to create your account. 
+"""
