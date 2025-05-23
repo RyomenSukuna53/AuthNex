b54ab4cfbfc60798f9721e3af2a45ca5088518f8
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-05-23 15:08:04 +0530

    Create __init__.py

---
+++ AuthNex/__init__.py
@@ -0,0 +1,21 @@
+from pyrogram import Client 
+from config import *
+
+AuthNex = Client(api_id=API_ID, 
+                 api_hash=API_HASH, 
+                 bot_token=BOT_TOKEN, 
+                 plugins=dict(root="(AuthNex/Modules)") 
+                ) 
+
+  import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[KURO-ZONE] %(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("bot.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)                            
