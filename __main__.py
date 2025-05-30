7b7174bdb1b6a87188830e725b4e080299550401
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-05-30 20:56:33 +0530

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,20 @@
+from AuthNex import app as AuthNex
+from pyrogram import Client 
+
+from AuthNex.Modules.start import start 
+AuthNex.add_handler(start) 
+
+from AuthNex.Modules.register import acc_start 
+AuthNex.add_handler(acc_start) 
+
+from AuthNex.Modules.register import acc_start 
+AuthNex.add_handler(acc_steps) 
+
+from AuthNex.Modules.delete import del_acc 
+AuthNex.add_handler(del_acc) 
+
+from AuthNex.Modules.all_logins import all_logins 
+AuthNex.add_handler(all_logins) 
+
+if __name__=="__main__":
+  AuthNex.run() 
