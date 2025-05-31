4ca58d5b8aeeb79fd8d18a670e6637634cc89430
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-05-31 09:23:57 +0530

    Update logs.py

--- AuthNex/Modules/logs.py
+++ AuthNex/Modules/logs.py
@@ -9,13 +9,13 @@ from pyrogram.handlers import MessageHandler
 
 async def logs(_, message: Message):
     if message.from_user.id == 6239769036:
-      run_logs = run("tail log.txt")
-      text = await message.reply_text("`Getting logs...`")
-      await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
-      await message.reply_text(f"```shell\n{run_logs}```")
-      await text.delete()
+        run_logs = run("tail log.txt")
+        text = await message.reply_text("`Getting logs...`")
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        await message.reply_text(f"```shell\n\n{run_logs}```")
+        await text.delete()
     else: 
-      return
+        return
 
 logs = MessageHandler(logs, filters.command("botlogs") & (filters.private | filters.group)) 
 
