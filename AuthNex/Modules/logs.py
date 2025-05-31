849a37b6fa3ec6683442892b29147cbf6f6ac990
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-05-31 08:23:08 +0530

    Create logs.py

---
+++ AuthNex/Modules/logs.py
@@ -0,0 +1,23 @@
+import io
+from pyrogram import *
+from AuthNex import AuthNex as bot
+import traceback
+from subprocess import getoutput as run
+from pyrogram.enums import ChatAction
+from pyrogram.types import Message 
+from pyrogram.handlers import MessageHandler
+
+@bot.on_message(filters.command(["logs", "log"]))
+async def logs(_, message: Message):
+    if message.from_user.id == 6239769036:
+      run_logs = run("tail log.txt")
+      text = await message.reply_text("`Getting logs...`")
+      await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+      await message.reply_text(f"```shell\n{run_logs}```")
+      await text.delete()
+    else: 
+      return 
+
+
+
+
