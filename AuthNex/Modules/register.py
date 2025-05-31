b75c88929ebefe2920f8439dc1c2e5297330b821
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-05-31 08:16:57 +0530

    Update register.py

--- AuthNex/Modules/register.py
+++ AuthNex/Modules/register.py
@@ -74,7 +74,8 @@ async def handle_register_step(_, message: Message):
     elif state["step"] == "username":
         if not text.startswith("$") or " " in text:
             return await message.reply("⚠️ Username must start with `$` and contain no spaces.")
-        if user_col.find_one({"username": text}):
+        _er = await user_col.find_one({"username": text}):
+        if _er:
             return await message.reply("⚠️ Username already exists, try another.")
         state["username"] = text
 
