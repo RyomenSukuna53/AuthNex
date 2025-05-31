5147afb6f42a0d8eed0ebfe34f111f1096ce7a96
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-05-31 08:06:01 +0530

    Update all_logins.py

--- AuthNex/Modules/all_logins.py
+++ AuthNex/Modules/all_logins.py
@@ -1,21 +1,20 @@
 from pyrogram import filters
-from AuthNex import app  # assuming this is your Client
+from AuthNex import app  # Your custom Pyrogram Client from AuthNex
 from AuthNex.Database import user_col
-from pyrogram.types import Message 
-from pyrogram.handlers import MessageHandler 
+from pyrogram.types import Message
+from pyrogram.handlers import MessageHandler
 
 SUDO_USERS = [6239769036]  # Replace with your Telegram user ID(s)
 
-
 async def all_logins(_, message: Message):
-	users = user_col.find()
-	if not await user_col.count_documents({}):
-		return await message.reply_text("[ℍ𝗢𝕊𝗧] ==> No user accounts found.")
+    users = user_col.find()
+    if not await user_col.count_documents({}):
+        return await message.reply_text("[ℍ𝗢𝕊𝗧] ==> No user accounts found.")
 
-	reply =  "**[ℍ𝗢𝕊𝗧] ==> All Registered Users:**\n\n"
-	for user in users:
-		reply += (
-		    f"• **ID:** `{user.get('_id')}`\n"
+    reply = "**[ℍ𝗢𝕊𝗧] ==> All Registered Users:**\n\n"
+    async for user in users:
+        reply += (
+            f"• **ID:** `{user.get('_id')}`\n"
             f"• **Name:** `{user.get('name')}`\n"
             f"• **Age:** `{user.get('age')}`\n"
             f"• **Mail:** `{user.get('mail')}`\n"
@@ -23,19 +22,15 @@ async def all_logins(_, message: Message):
             f"• **Password:** `{user.get('password')}`\n"
             f"----------------------------\n\n"
         )
-        
+
+    # Send in chunks if too long
     if len(reply) > 4096:
-		# Break long text into chunks
         for i in range(0, len(reply), 4096):
-			await message.reply_text(reply[i:i+4096], disable_web_page_preview=True)
+            await message.reply_text(reply[i:i+4096], disable_web_page_preview=True)
     else:
-	    await message.reply_text(reply)
+        await message.reply_text(reply)
 
+# Register the handler
+all_logins = MessageHandler(all_logins, filters.command("all_logins") & (filters.private | filters.group) & filters.user(SUDO_USERS))
 
-all_logins = MessageHandler(all_logins, filters.command("all_logins") & (filters.private | filters.group) & filters.user(SUDO_USERS)) 
-
-
-
-
-			    
 
