ecb25669ff4d6a3ab1180b647517247eddbdeac9
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-06-01 21:57:06 +0530

    Update all_logins.py

--- AuthNex/Modules/all_logins.py
+++ AuthNex/Modules/all_logins.py
@@ -4,12 +4,11 @@ from AuthNex.Database import user_col
 from pyrogram.types import Message
 from pyrogram.handlers import MessageHandler
 
-SUDO_USERS = [6239769036]  # Replace with your Telegram user ID(s)
 
-async def all_logins(_, message: Message):
-    users = await user_col.find()
+async def all_logins(_, m: Message):
+    users = await user_col.find("_id": None)
     if not await user_col.count_documents({}):
-        return await message.reply_text("[ℍ𝗢𝕊𝗧] ==> No user accounts found.")
+        return await m.reply_text("[ℍ𝗢𝕊𝗧] ==> No user accounts found.")
 
     reply = "**[ℍ𝗢𝕊𝗧] ==> All Registered Users:**\n\n"
     async for user in users:
@@ -20,14 +19,11 @@ async def all_logins(_, message: Message):
             f"• **Mail:** `{user.get('mail')}`\n"
             f"• **Username:** `{user.get('username')}`\n"
             f"• **Password:** `{user.get('password')}`\n"
-            f"----------------------------\n\n"
-        )
-
-    # Send in chunks if too long
-    await message.reply_text(reply)
+            f"--------------------------------")
+        await m.reply_text(reply)
 
 # Register the handler
-all_logins = MessageHandler(all_logins, filters.command("all_acc") & (filters.private | filters.group) & filters.user(SUDO_USERS))
+all_logins = MessageHandler(all_logins, filters.command("all_acc") & (filters.private | filters.group) & filters.user(6239769036))
 
 
 
