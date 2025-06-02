4b3e45937696a0ef6e98a720c14b69f55a1d1824
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-06-02 15:02:36 +0530

    Update accounts.py

--- AuthNex/Modules/accounts.py
+++ AuthNex/Modules/accounts.py
@@ -1,33 +1,39 @@
-from pyrogram import Client, filters
-from pyrogram.enums import ChatType, ParseMode
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from AuthNex import app
-from AuthNex.Database import user_col
-import random
-import asyncio
+from pyrogram import filters
+from pyrogram.types import Message
 from pyrogram.handlers import MessageHandler
+from AuthNex.Database import user_col
+from AuthNex import app
 
 SUDO_USER = [6239769036]
 
-async def accounts(_, m: Message):
-    users = user_col.find_many({"_id": None}) 
+async def accounts_handler(_, m: Message):
+    users = user_col.find({})  # Fetch all users
 
-    if not user:
-      await m.reply("😭")
-      await m.reply("𝗡𝗼 𝗜𝗗'𝗦 𝗳𝗼𝘂𝗻𝗱.") 
-      return 
+    # Check if users exist
+    if not await user_col.count_documents({}):
+        await m.reply("😭")
+        await m.reply("𝗡𝗼 𝗜𝗗'𝗦 𝗳𝗼𝘂𝗻𝗱.") 
+        return 
 
     reply = "🗝 𝗔𝗹𝗹 𝗿𝗲𝗴𝗶𝘀𝘁𝗲𝗿𝗲𝗱 𝘂𝘀𝗲𝗿𝘀 𝗹𝗶𝘀𝘁 💳\n\n"
     for user in users:
-      reply += f"**𝗡𝗔𝗠𝗘:** {user.get('Name')}\n"
-               f"**AGE:** {user.get('Age')}\n"
-               f"**𝗔𝗨𝗧𝗛-𝗠𝗔𝗜𝗟:** {user.get('Mail')}\n" 
-               f"**𝗣𝗔𝗦𝗦𝗪𝗢𝗥𝗗:** {user.get('Password')}\n" 
-               f"**ID:** {user.get('_id')}\n"
-               f"**AUTH-COINS:** {user.get('Authcoins')}\n"
-               f"**OWNER:** {user.get('Owner')}\n\n"
-               "----------------------------------\n\n"
+        reply += (
+            f"**𝗡𝗔𝗠𝗘:** {user.get('Name', 'N/A')}\n"
+            f"**AGE:** {user.get('Age', 'N/A')}\n"
+            f"**𝗔𝗨𝗧𝗛-𝗠𝗔𝗜𝗟:** {user.get('Mail', 'N/A')}\n" 
+            f"**𝗣𝗔𝗦𝗦𝗪𝗢𝗥𝗗:** {user.get('Password', 'N/A')}\n" 
+            f"**ID:** {user.get('_id', 'N/A')}\n"
+            f"**AUTH-COINS:** {user.get('Authcoins', 'N/A')}\n"
+            f"**LOGINED:** {user.get('Login')}\n"
+            f"**OWNER:** {user.get('Owner', 'N/A')}\n\n"
+            "----------------------------------\n\n"
+        )
 
-    await m.reply(reply) 
+    await m.reply(reply, quote=True)
 
-accounts = MessageHandler(accounts, filters.command('accounts') & (filters.group | filters.private) & filters.user(SUDO_USER)) 
+accounts_handler_obj = MessageHandler(accounts_handler, filters.command('accounts') & (filters.group | filters.private) & filters.user(SUDO_USER)
+)
+
+# Don't forget to add this in your main file:
+# app.add_handler(accounts_handler_obj)
+
