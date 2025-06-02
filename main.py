d7837527fe52845c79ec657b77059b5cdf9640cc
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-06-02 20:47:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from AuthNex import app as AuthNex
-from pyrogram import Client 
-from AuthNex.Modules.start import start 
+from pyrogram import Client
+from AuthNex.Modules.start import start
 from AuthNex.Modules.register import acc_start
-from AuthNex.Modules.reset import ResetHandlerObject 
+from AuthNex.Modules.reset import ResetHandlerObject
 from AuthNex.Modules.register import acc_steps
 from AuthNex.Modules.accounts import accounts_handler_obj
+from AuthNex.Modules.logs import logs
 
-
-AuthNex.add_handler(start) 
-AuthNex.add_handler(acc_start) 
-AuthNex.add_handler(acc_steps) 
-AuthNex.add_handler(ResetHandlerObject) 
-AuthNex.add_handler(accounts_handler_obj) 
+AuthNex.add_handler(start)
+AuthNex.add_handler(acc_start)
+AuthNex.add_handler(acc_steps)
+AuthNex.add_handler(ResetHandlerObject)
+AuthNex.add_handler(accounts_handler_obj)
+AuthNex.add_handler(logs) 
 
 
 if __name__=="__main__":
