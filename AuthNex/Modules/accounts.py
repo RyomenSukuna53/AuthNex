a372841219d4f355265b05c580ab5ec4d6c896ec
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-06-02 16:04:41 +0530

    Update accounts.py

--- AuthNex/Modules/accounts.py
+++ AuthNex/Modules/accounts.py
@@ -1,8 +1,33 @@
 from pyrogram import filters
-from pyrogram.types import Message
-from pyrogram.handlers import MessageHandler
-from AuthNex.Database import user_col
 from AuthNex import app
+from AuthNex.Database import user_col
+from pyrogram.handlers import MessageHandler
+
+SUDO_USER = [6239769036]
+
+async def accounts(_, m: Message):
+    users = user_col.find_many({"_id": None}) 
+
+    if not user:
+        await m.reply("😭")
+        await m.reply("𝗡𝗼 𝗜𝗗'𝗦 𝗳𝗼𝘂𝗻𝗱.") 
+        return 
+
+    reply = "🗝 𝗔𝗹𝗹 𝗿𝗲𝗴𝗶𝘀𝘁𝗲𝗿𝗲𝗱 𝘂𝘀𝗲𝗿𝘀 𝗹𝗶𝘀𝘁 💳\n\n"
+    for user in users:
+        reply += f"**𝗡𝗔𝗠𝗘:** {user.get('Name')}\n"
+                 f"**AGE:** {user.get('Age')}\n"
+                 f"**𝗔𝗨𝗧𝗛-𝗠𝗔𝗜𝗟:** {user.get('Mail')}\n" 
+                 f"**𝗣𝗔𝗦𝗦𝗪𝗢𝗥𝗗:** {user.get('Password')}\n" 
+                 f"**ID:** {user.get('_id')}\n"
+                 f"**AUTH-COINS:** {user.get('Authcoins')}\n"
+                 f"**OWNER:** {user.get('Owner')}\n\n"
+                 "----------------------------------\n\n"
+
+    await m.reply(reply) 
+
+accounts = MessageHandler(accounts, filters.command('accounts') & (filters.group | filters.private) & filters.user(SUDO_USER)) 
+
 
 SUDO_USER = [6239769036]
 
