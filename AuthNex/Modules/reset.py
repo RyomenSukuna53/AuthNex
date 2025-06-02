766309cfad9a191e912551cfd2f5f5c15b5b7d50
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-06-02 19:56:23 +0530

    Update reset.py

--- AuthNex/Modules/reset.py
+++ AuthNex/Modules/reset.py
@@ -1,12 +1,12 @@
 from pyrogram import Client, filters
 from pyrogram.enums import ChatType, ParseMode
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from AuthNex import app as AuthNex 
-from AuthNex.Database import user_col
 import random
 import asyncio
 from pyrogram.handlers import MessageHandler
 from config import * 
+from AuthNex import app as AuthNex 
+from AuthNex.Database import user_col
 
 
 # Reset command function
