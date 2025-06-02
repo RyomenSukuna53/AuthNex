c76cb0b201fbf5f899b8110e8d38178b24ecb294
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-06-02 06:40:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 API_ID = int(os.getenv("API_ID", 27548865)) 
 API_HASH = os.getenv("API_HASH", "db07e06a5eb288c706d4df697b71ab61") 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8008184262:AAGWmpK1_B9crHIleg9s2dgN2F_7dzGZqaQ")   # <-- Yahan apna bot token daalna
+SUDO = [6239769036, 7576729648]
