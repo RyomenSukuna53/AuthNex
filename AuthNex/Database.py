0cbe98706283c0b1fc262b50b9c122c214bc0515
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-06-04 15:51:32 +0530

    Delete AuthNex directory

--- AuthNex/Database.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient 
-
-MONGO_URI = "mongodb+srv://sufyan532011:2011@authnex.nvjbscr.mongodb.net/?retryWrites=true&w=majority&appName=AuthNex"
-Database = AsyncIOMotorClient(MONGO_URI)
-AuthNex = Database["AuthNex"]
-user_col = AuthNex["USER_LOGINS"]
-ban_col = AuthNex["BANNED_USERS"]
-sessions_col = AuthNex["SESSIONS"]
--- AuthNex/__main__.py
+++
@@ -1,6 +0,0 @@
-from AuthNex import app
-from AuthNex.Modules import *
-from AuthNex.Database import *
-
-if __name__=="__main__":
-    app.run()
