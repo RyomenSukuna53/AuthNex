d00d45e198b8f8a399e2fb049182c3cd84705535
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-06-04 20:13:07 +0530

    Update login.py

--- AuthNex/login.py
+++ AuthNex/login.py
@@ -12,7 +12,7 @@ async def start_login(_, message: Message):
     login_state[user_id] = {"step": "mail"}
     await message.reply("📧 Please enter your mail to login:")
 
-@Client.on_message(filters.text & (filters.private), group=9) 
+@Client.on_message(filters.text & (filters.private)) 
 async def handle_login_input(_, message: Message):
     user_id = message.from_user.id
     if user_id not in login_state:
