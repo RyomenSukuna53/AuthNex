aac94be22ab1e74d59902d9415f33fbcb94c2f41
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-06-04 15:59:27 +0530

    Create __main__.py

---
+++ AuthNex/__main__.py
@@ -0,0 +1,6 @@
+from AuthNex import app
+from AuthNex import start, register,login,logout, reset,profile,accounts,generatetoken
+from AuthNex import Database
+
+if __name__=="__main__":
+        app.run()
