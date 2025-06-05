6fc09dce3c53213b8e58c853f520776ed665ec5d
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-06-05 09:12:37 +0530

    Update Authentication.py

--- AuthNex/Modules/Authentication.py
+++ AuthNex/Modules/Authentication.py
@@ -1,4 +1,10 @@
+import random
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.enums import ChatType, ParseMode
-from AuthNex import app
+from AuthNex.Modules import HelperBot
+
+async def code_generator(_, m: Message):
+    code = random.randint(10000, 99999)
+    _id = m.from_user.id
+    await Client.send_message(chat_id=_id, text=`"Your Authentication Code: {code}`")
