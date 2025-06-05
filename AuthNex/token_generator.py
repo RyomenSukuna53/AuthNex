fa068efe0991967cf8a81f844604c8f31a6dbb09
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-06-05 21:47:23 +0530

    Update token_generator.py

--- AuthNex/token_generator.py
+++ AuthNex/token_generator.py
@@ -5,6 +5,7 @@ from AuthNex.Database import user_col, sessions_col, ban_col, tokens_col
 import secrets
 from asyncio import sleep
 from pyrogram.enums import ChatType, ParseMode
+import asyncio
 
 async def generate_authnex_token(length=50):
     return secrets.token_hex(length // 2)  # length in hex digits
@@ -18,6 +19,8 @@ async def token_generator(Client, message: Message):
     if message.chat.type == ChatType.GROUP:
         return await message.reply("USE IN DM")
     session = await sessions_col.find_one({"_id": user_id})
+    mail = session.get('mail')
+    user = user_col.find_one({"Mail": mail})
     if not session:
         return
     token = await tokens_col.find_one({"_id": user_id})
@@ -27,6 +30,10 @@ async def token_generator(Client, message: Message):
         token = await generate_authnex_token()
         if tokens_col.find_one({"token": token}):
             break
+    await message.reply("•--> 𝗘𝗻𝘁𝗲𝗿 𝘆𝗼𝘂𝗿 𝗽𝗮𝘀𝘀𝘄𝗼𝗿𝗱 𝘁𝗼 𝗰𝗼𝗻𝗳𝗶𝗿𝗺")
+    password = message.text
+    if password != user.get('Password'):
+        return await message.reply("Wrong pass try again after some time.")
     await message.reply("🔑 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐢𝐧𝐠 𝐓𝐨𝐤𝐞𝐧...")
     await asyncio.sleep(1)
     await message.delete()
