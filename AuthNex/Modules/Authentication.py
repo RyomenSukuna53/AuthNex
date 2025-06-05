603f2055c2c9e0e27e5e2824e101e7e3d32bbc52
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-06-05 15:14:10 +0530

    Update Authentication.py

--- AuthNex/Modules/Authentication.py
+++ AuthNex/Modules/Authentication.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 import random
-from AuthNex import HelperBot
+from bot import auth_bot
 
 otp_storage = {}
 
@@ -8,7 +8,7 @@ async def authentication_code(mail: str, target_id: int):
     code = random.randint(10000, 99999)
     otp_storage[target_id] = {"mail": mail, "code": str(code)}
 
-    await HelperBot.send_message(
+    await auth_bot.send_message(
         chat_id=target_id,
         text=f"🔐 Your authentication code for `{mail}` is: **{code}**"
     )
