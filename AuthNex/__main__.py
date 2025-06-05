137618e67e2f8c6f93bb1a001bd8b3dc9cf6c290
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-06-05 16:40:34 +0530

    Update __main__.py

--- AuthNex/__main__.py
+++ AuthNex/__main__.py
@@ -2,6 +2,6 @@ from AuthNex import app, auth_bot
 from AuthNex import start, register, login, logout, reset, profile, accounts, token_generator
 from AuthNex import Database
 
-if __name__="__main__":
+if __name__=="__main__":
     app.run()
     
