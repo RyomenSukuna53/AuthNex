db36111d175069043715859ecc00b1e4f1d15289
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2025-06-09 15:51:08 +0530

    Create broadcast.py

---
+++ AuthNex/broadcast.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from AuthNex import app
+from AuthNex.Database import sessons_col
+from config import SUDO
+
+@Client.on_message(filters.commmand["broadcast", "bcast"] & filters.private & filters.user(SUDO), group=26)
+async def broadcast_by_KURORAIJIN(_, m: Message):
+    users = await user_col.find_many({"_id": None})
+    msg = m.text[1:]
+    await m.reply("BROADCAST STARTED...")
+    for user in users:
+        await Client.send_message(user, msg)
