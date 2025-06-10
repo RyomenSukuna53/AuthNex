48e0c3740802f6397bc6f8259ddb168ca86d417e
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2025-06-10 19:51:26 +0530

    Update refer.py

--- AuthNex/refer.py
+++ AuthNex/refer.py
@@ -8,6 +8,6 @@ async def rrefer_someone(_, message):
     session = data.sessions_col.find_one({"_id": _id})
     if not session:
         return await message.reply("No login found for ID")
-    Link = f"https://t.me/AuthNexBot?start={user}"d
+    Link = f"https://t.me/AuthNexBot?start={user}"
     await message.reply(f"**__This is your WARRIOR reffer link\nUSE it to refer someone and get coins__**\n>{Link}")
     pass
