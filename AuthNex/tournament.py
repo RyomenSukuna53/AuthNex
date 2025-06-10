25075766dafd627d73f099486b1073372475f9db
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2025-06-10 20:07:26 +0530

    Update tournament.py

--- AuthNex/tournament.py
+++ AuthNex/tournament.py
@@ -4,7 +4,7 @@ from pyrogram.enums import ChatType, ParseMode
 from AuthNex import app
 from AuthNex.Datbase import JoinedPlayers, sessions_col, user_col
 import config
-@Client.on_message(filter.command("startuor") & filters.user(config.SUDO))
+@Client.on_message(filter.command("star_tuor") & filters.user(config.SUDO))
 async def TUORNAMENT(_, m: Message):
     _id = m.from_user.id
     session = sessions_col.find_one({"_id": _id})
@@ -13,7 +13,7 @@ async def TUORNAMENT(_, m: Message):
     user = user_col.find_one({"Mail": session.get("mail")})
     if user.get("AuthCoins", 0)<1000:
         return await m.reply("NOT ENOUGH COINS TO CONFIRM")
-    if not user.get("TCA": 0)=0:
+    if not user.get("TCA", 0)=0:
         return await m.reply("YOU DONT HAVE AUTHENTICATION TOURNAMENT PERTIT PLZ PURCHASE I FIRST OR FOUND IT IN A GROK DOUNGEN")
     await m.reply(">**YOU HAVE TO GIVE YOUR PERMIT AND 1000 COINS TO START", reply_markup=InlineKeyboardMarkup([
         InlineKeyboardButton("PAY", callback_data=f"pay_{_id}")
