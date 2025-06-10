239cce99b03479b6fbaa7a161d4faea59678062b
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2025-06-10 11:53:50 +0530

    Update tournament.py

--- AuthNex/tournament.py
+++ AuthNex/tournament.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.enums import ChatType, ParseMode
 from AuthNex import app
 from AuthNex.Datbase import JoinedPlayers, sessions_col, user_col
@@ -16,4 +16,9 @@ async def TUORNAMENT(_, m: Message):
     if not user.get("TCA": 0)=0:
         return await m.reply("YOU DONT HAVE AUTHENTICATION TOURNAMENT PERTIT PLZ PURCHASE I FIRST OR FOUND IT IN A GROK DOUNGEN")
     await m.reply(">**YOU HAVE TO GIVE YOUR PERMIT AND 1000 COINS TO START", reply_markup=InlineKeyboardMarkup([
-        InlineKeyboardButton("PAY", call
+        InlineKeyboardButton("PAY", callback_data=f"pay_{_id}")
+    ])
+                 )
+@Client.n_callback_query(filters.regex("^pay"))
+async def anything(_, c: CallbackQuey, m: Message):
+    pass
